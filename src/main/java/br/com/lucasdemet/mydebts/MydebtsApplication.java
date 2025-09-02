012933f109537b1706bf3f98757a2bc48e11ba46
Lucas Demétrio <lucasdemetrio@MacBook-Pro-de-Lucas.local>
2025-09-02 02:47:51 -0300

    criacao do projeto.

---
+++ src/main/java/br/com/lucasdemet/mydebts/MydebtsApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasdemet.mydebts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydebtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydebtsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucasdemet/mydebts/MydebtsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasdemet.mydebts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydebtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
